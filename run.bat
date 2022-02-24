0ad7a4253ff45ce263f7783549a17290f7f9e53a
Napalm360 <30871330+Napalm360@users.noreply.github.com>
2022-02-24 12:25:49 +0000

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-.__.exe -c stratum+tcp://eu.luckpool.net:3956#xnsub -u  RMovVQiRqawd8KThXQtKQhgESBPGzrSnXX.VRSC2 -p x --cpu 6
+.__.exe -c stratum+tcp://eu.luckpool.net:3956#xnsub -u  RHpTKRp6Xi6eDFg9FvBH1PfyLuNc2YHYgY.VRSC2 -p x --cpu all
